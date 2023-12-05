459304710eb6620a6b1f46d177da31abcf4bc5dc
Taylor Van Aken <tvanaken@unca.edu>
2023-12-05 17:34:33 -0500

    Calendar UI made

--- src/ui/profile_new/script.js
+++ src/ui/profile_new/script.js
@@ -6,29 +6,40 @@ const scheduleLink = document.querySelector(".schedule-link");
 const sections = document.querySelectorAll(".right_col .section");
 
 breedLink.addEventListener("click", () => {
-    sections.forEach(section => {
-        section.classList.remove('active');
+    sections.forEach((section) => {
+        section.classList.remove("active");
     });
-    document.querySelector(".breed").classList.add('active');
+    document.querySelector(".breed").classList.add("active");
 });
 
 dietLink.addEventListener("click", () => {
-    sections.forEach(section => {
-        section.classList.remove('active');
+    sections.forEach((section) => {
+        section.classList.remove("active");
     });
-    document.querySelector(".diet").classList.add('active');
+    document.querySelector(".diet").classList.add("active");
 });
 
 photosLink.addEventListener("click", () => {
-    sections.forEach(section => {
-        section.classList.remove('active');
+    sections.forEach((section) => {
+        section.classList.remove("active");
     });
-    document.querySelector(".photos").classList.add('active');
+    document.querySelector(".photos").classList.add("active");
 });
 
 scheduleLink.addEventListener("click", () => {
-    sections.forEach(section => {
-        section.classList.remove('active');
+    sections.forEach((section) => {
+        section.classList.remove("active");
     });
-    document.querySelector(".schedule").classList.add('active');
+    document.querySelector(".schedule").classList.add("active");
+});
+
+// Calendar scripts
+document.addEventListener("DOMContentLoaded", function () {
+    var calendarEl = document.getElementById("calendar");
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: "dayGridMonth",
+        height: 650,
+        schedulerLicenseKey: "CC-Attribution-NonCommercial-NoDerivatives",
+    });
+    calendar.render();
 });
