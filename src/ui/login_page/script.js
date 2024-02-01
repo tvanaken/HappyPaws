fd0ec2ddaa4dd53ec554abf1a7c85b1538319078
Taylor Van Aken <tvanaken@unca.edu>
2024-02-01 17:51:57 -0500

    To other pc

--- src/ui/login_page/script.js
+++ src/ui/login_page/script.js
@@ -9,3 +9,21 @@ registerLink.addEventListener("click", () => {
 loginLink.addEventListener("click", () => {
     wrapper.classList.remove("active");
 });
+
+document
+    .getElementById("signupForm")
+    .addEventListener("submit", async function (event) {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        const response = await fetch("http://localhost:8000/api/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        const result = await response.json();
+    });
